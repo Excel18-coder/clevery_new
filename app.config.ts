4345c4cbf239fe9dd78ef238e42326c0aa6734b9
larrybwosi <larrybwosi@gmail.com>
2024-09-29 16:39:37 +0300

    v3 commit

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Clevery',
   slug: 'clevery',
-  version: '2.0.0',
+  version: '3.0.0',
   orientation: 'portrait',
   icon: './src/assets/images/icon.png',
   scheme: 'com.clevery.app',
